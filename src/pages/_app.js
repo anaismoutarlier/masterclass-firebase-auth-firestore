6950f215ba51bcda15a01b70a6f97e82ccf21bfe
Anais Moutarlier <anaismoutarlier@gmail.com>
2024-09-26 19:44:32 +0200

    instant messaging with Cloud Firestore

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -7,6 +7,14 @@ import {
   GoogleAuthProvider,
   signOut,
 } from "firebase/auth";
+import {
+  getFirestore,
+  addDoc,
+  collection,
+  query,
+  onSnapshot,
+  orderBy,
+} from "firebase/firestore";
 import { firebaseConfig } from "@/firebase/config";
 import { FirebaseContext } from "@/firebase/firebase.context";
 import { useEffect, useState } from "react";
@@ -15,16 +23,19 @@ const googleAuthProvider = new GoogleAuthProvider();
 
 export default function App({ Component, pageProps }) {
   const [auth, setAuth] = useState(null);
+  const [db, setDb] = useState(null);
   const [user, setUser] = useState(null);
+  const [messages, setMessages] = useState([]);
+
   useEffect(() => {
     const app = initializeApp(firebaseConfig);
     setAuth(getAuth(app));
+    setDb(getFirestore(app));
   }, []);
 
   useEffect(() => {
     if (auth) {
       const unsubscribe = auth.onAuthStateChanged(data => {
-        console.log(data);
         setUser(data);
       });
 
@@ -34,12 +45,46 @@ export default function App({ Component, pageProps }) {
     }
   }, [auth]);
 
+  useEffect(() => {
+    if (db) {
+      const q = query(collection(db, "messages"), orderBy("sentAt"));
+      const unsubscribe = onSnapshot(q, data => {
+        const messages = data.docs.map(doc => {
+          const data = doc.data();
+          data.sentAt = data.sentAt.toDate();
+          return { id: doc.id, ...data };
+        });
+        setMessages(messages);
+      });
+
+      return () => {
+        unsubscribe();
+      };
+    }
+  }, [db]);
+
   const signin = async () => await signInWithPopup(auth, googleAuthProvider);
 
   const signout = async () => await signOut(auth);
 
+  const sendMessage = async content => {
+    if (!content || !user || !db) return;
+    const message = {
+      content,
+      user: {
+        id: user.uid,
+        displayName: user.displayName,
+        photoURL: user.photoURL,
+      },
+      sentAt: new Date(),
+    };
+    await addDoc(collection(db, "messages"), message);
+  };
+
   return (
-    <FirebaseContext.Provider value={{ signin, user, signout }}>
+    <FirebaseContext.Provider
+      value={{ signin, user, signout, sendMessage, messages }}
+    >
       <Component {...pageProps} />
     </FirebaseContext.Provider>
   );
